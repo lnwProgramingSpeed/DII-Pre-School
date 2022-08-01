a0db7423dc6e6249b9870b4f7c6a6d8529fd9f70
Ahhhhman <71209176+lnwProgramingSpeed@users.noreply.github.com>
2022-08-02 00:37:55 +0700

    Add files via upload

--- Lab4/EvenOdd.java
+++ Lab4/EvenOdd.java
@@ -3,23 +3,34 @@ import java.util.Scanner;
 public class EvenOdd {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int randomNum = 0;
-        int sumOdd = 0;
-        int sumEven = 0;
-        char s = (char) randomNum;
-        while (s != 'Z') {
+
+        double sumOdd = 0;
+        double sumEven = 0;
+
+        while (true) {
             System.out.print("Input number: ");
-            randomNum = scan.nextInt();
+            char s = scan.next().charAt(0);
+
+            if (s == 'Z') {
+                break;
+            }
+
+            double randomNum = s;
+            randomNum = randomNum - 48;
+            //Char change into int make them binary form so minus 48 which is 0 in int made them a round number
 
             if (randomNum % 2 == 0) {
                 sumEven = sumEven + randomNum;
-            } else {
+
+            } else if (randomNum % 2 != 0) {
                 sumOdd = sumOdd + randomNum;
+
             }
+
         }
 
-        System.out.print(sumEven);
-        System.out.print(sumOdd);
+        System.out.println(sumEven);
+        System.out.println(sumOdd);
 
     }
 }
