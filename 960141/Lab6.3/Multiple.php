aea39c533dcb38fec7a51ebeb065c6ac10744e0c
Ahhhhman <71209176+lnwProgramingSpeed@users.noreply.github.com>
2022-09-05 08:31:54 +0700

    Add files via upload

--- 960141/Lab6.3/Multiple.php
+++ 960141/Lab6.3/Multiple.php
@@ -1,7 +1,7 @@
 <?php
     $NUM = $_POST['num'];
 
-    for (int $i = 1; $i<=$NUM; $i++){
-        echo "$i * 12 = ".$i*12."<br>";
+    for (int $i = 1; $i<=12; $i++){
+        echo "$NUM * $i = ".$i*$NUM."<br>";
     }
 ?>
